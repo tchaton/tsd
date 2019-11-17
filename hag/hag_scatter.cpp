cd7a07597802c3cb2fc1638e7243443c929a02cf
Thomas Chaton <thomas.chaton.ai@gmail.com>
2019-11-17 21:26:46 +0000

    'scatter_max is wrong'

--- hag/hag_scatter.cpp
+++ hag/hag_scatter.cpp
@@ -7,12 +7,55 @@
 void scatter_max_(std::map<V_ID, std::set<V_ID>*>& orgList,
                     std::map<V_ID, std::set<V_ID>*>& optList,
                     std::vector<std::pair<V_ID, V_ID> >& ranges,
-                    V_ID nv, at::Tensor src, at::Tensor out, int64_t dim){
-
+                    V_ID nv, at::Tensor src, at::Tensor out, int64_t dim, int64_t maxWidth){
+  at::Tensor tmp_ = torch::zeros({maxWidth, src.size(-1)}, torch::TensorOptions().dtype(torch::kFloat));
+  for (int i = 0; i < ranges.size(); i++){
+    V_ID rowLeft = ranges[i].first;
+    V_ID rowRight = ranges[i].second;
+    //std::cout << rowLeft << " " << rowRight << std::endl;
+    for (V_ID j = rowLeft; j <= rowRight; j++){
+      std::set<V_ID>::const_iterator it, first = orgList[i]->begin(),
+                                    last = orgList[i]->end();
+      at::Tensor tmp_element;
+      for (it = first; it != last; it++){
+        if (it == first){
+          tmp_element = src[*it];
+        }else{
+          tmp_element = at::cat({tmp_element, src[*it]});
+        }
+      }
+      std::tuple<at::Tensor, at::Tensor> max_ = tmp_element.max(0, false);
+      tmp_[j - nv] = std::get<0>(max_);
+    }
+  }
+  for (int i = 0; i < nv; i++){
+    std::set<V_ID>::const_iterator it, first = orgList[i]->begin(),
+                                   last = orgList[i]->end();  
+    at::Tensor tmp_element;
+    for (it = first; it != last; it++){
+      if (it == first){
+        if (*it > nv){
+          tmp_element = tmp_[*it - nv];
+        }else{
+          tmp_element =  src[*it];
+        }        
+      }else{
+        if (*it > nv){
+          tmp_element = at::cat({tmp_element, tmp_[*it - nv]});
+        }else{
+          tmp_element = at::cat({tmp_element, src[*it]});
+        }
+      }
+    }
+    std::tuple<at::Tensor, at::Tensor> max_ = tmp_element.max(0, false);
+    out[i] = std::get<0>(max_);
+  }
 }
 
-void scatter_max(at::Tensor src, at::Tensor edge_indexes, at::Tensor out, int64_t dim) {
+at::Tensor scatter_max(at::Tensor src, at::Tensor edge_indexes, at::Tensor out, int64_t dim) {
   std::map<V_ID, std::set<V_ID>*> inEdges;
+  V_ID nv = out.size(0);
+  //std::cout << nv << std::endl;
   V_ID maxNodeIndex = 0;
   E_ID numEdges = 0;
   V_ID maxDepth = 10;
@@ -23,7 +66,8 @@ void scatter_max(at::Tensor src, at::Tensor edge_indexes, at::Tensor out, int64_
   std::vector<std::pair<V_ID, V_ID> > optRanges;
   V_ID new_max_node_index;
   transfer_graph(inEdges, optInEdges, optRanges, maxNodeIndex, numEdges, maxDepth, maxWidth, new_max_node_index);
-  scatter_max_(inEdges, optInEdges, optRanges, new_max_node_index, src, out, dim);
+  scatter_max_(inEdges, optInEdges, optRanges, nv, src, out, dim, maxWidth);
+  return out;
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
--- test/test_hag_scatter.py
+++ test/test_hag_scatter.py
@@ -1,4 +1,6 @@
 from tsd.hag_scatter import scatter_max
+from time import time
+from tsd import scatter_max as sc_max
 import torch
 
 num_samples = 100
@@ -10,4 +12,12 @@ edge_j = torch.randint(0, num_samples, (edge_samples,)).unsqueeze(0)
 
 edge_indexes = torch.cat([edge_i, edge_j])
 
-output = scatter_max(input, edge_indexes, torch.zeros_like(input), 0)
+input_expanded = torch.index_select(input, 0, edge_indexes[0])
+
+t0 = time()
+output = scatter_max(input_expanded, edge_indexes, torch.zeros_like(input), 0)
+print(time() - t0, output[0])
+
+t0 = time()
+out, argmax = sc_max(input_expanded, edge_indexes[0], 0, fill_value=0)
+print(time() - t0, out[0])
